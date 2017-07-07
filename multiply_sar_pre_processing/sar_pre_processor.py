db88a717d173be06bfc79295cac3254bc46125f1
Tonio Fincke <tonio.fincke@brockmann-consult.de>
2017-07-07 16:56:15 +0200

    reformatting to comply with pep-8

--- multiply_sar_pre_processing/sar_pre_processor.py
+++ multiply_sar_pre_processing/sar_pre_processor.py
@@ -4,7 +4,9 @@ Wrapper module to launch preprocessor
 
 import os
 
+
 class PreProcessor(object):
+
     def __init__(self, **kwargs):
         self.config = kwargs.get('config', None)
         self._check()
@@ -17,7 +19,9 @@ class PreProcessor(object):
     def pre_process(self):
         assert False, 'Routine should be implemented in child class'
 
+
 class SARPreProcessor(PreProcessor):
+
     def __init__(self, **kwargs):
         super(SARPreProcessor, self).__init__(**kwargs)
 
@@ -25,57 +29,53 @@ class SARPreProcessor(PreProcessor):
 
         # xml-processing graph
         self.xmlgraphpath = '/media/tweiss/Work/python_code/MULTIPLY/pre_processing_Sentinel_1/xml_files/'
-        self.xmlgraph = 'preprocess_v01.xml'    # the XML config files should gfo somewhere in a specific directory in the multiply-core repo
+        # the XML config files should gfo somewhere in a specific directory in the multiply-core repo
+        self.xmlgraph = 'preprocess_v01.xml'
 
-    def pre_process(self, **kwargs):  # todo discuss if input/output specification part of processing or part of instantiation of the object itself
+    # todo discuss if input/output specification part of processing or part of instantiation of the object itself
+    def pre_process(self, **kwargs):
         """
-        Parameters
+        Keyword parameters
         ----------
-        inputfolder : str
-            name of directory where the downloaded SAR data is 
+        input : str
+            name of directory where the downloaded SAR data is
             located
-        outputfolder : str
+        output : str
             name of directory where to put results
         """
-        self.inputfolder = kwargs.get('input', None)
-        assert self.inputfolder is not None, 'ERROR: input folder not specified'
-        assert os.path.exists(self.inputfolder)
+        input_folder = kwargs.get('input', None)
+        assert input_folder is not None, 'ERROR: input folder not specified'
+        assert os.path.exists(input_folder)
 
-        self.outputfolder = kwargs.get('output', None)
-        assert self.outputfolder is not None, 'ERROR: Outputfolder needs to be specified'
-        if not os.path.exists(self.outputfolder):
-            os.makedirs(self.outputfolder)
+        output_folder = kwargs.get('output', None)
+        assert output_folder is not None, 'ERROR: output folder needs to be specified'
+        if not os.path.exists(output_folder):
+            os.makedirs(output_folder)
         
         # Name addition for processed data
         xml_addition = 'subset'
         
-        lowerright_y = self.config.region['lr']['lat']
-        upperleft_y = self.config.region['ul']['lat']
-        upperleft_x = self.config.region['ul']['lon']
-        lowerright_x = self.config.region['lr']['lon']
+        lower_right_y = self.config.region['lr']['lat']
+        upper_left_y = self.config.region['ul']['lat']
+        upper_left_x = self.config.region['ul']['lon']
+        lower_right_x = self.config.region['lr']['lon']
         # todo: how is it with coordinates that go across the datum line ??
 
         # Coordinates for subset area
-        area = self.get_area(lowerright_y, upperleft_y, upperleft_x, lowerright_x)
-        
-        
+        area = self.get_area(lower_right_y, upper_left_y, upper_left_x, lower_right_x)
+
         # o.k., now the rest of the preprocessor can be added here
         # to keep things most flexible it would be good to have that in 
         # a separate class
         
         return 'some data'
 
-    def get_area(self, latmin, latmax, lonmin, lonmax):
+    def get_area(self, lat_min, lat_max, lon_min, lon_max):
         """
         Change input coordinates for subset operator
         """
-        assert latmin <= latmax, 'ERROR: invalid lat'
-        assert lonmin <= lonmax, 'ERROR: invalid lon'
-        return '%.14f %.14f,%.14f %.14f,%.14f %.14f,%.14f %.14f,%.14f %.14f' % (lonmin, latmin, lonmin, latmax, lonmax, latmax, lonmax, latmin, lonmin, latmin)
-
-
-
-
-
-
-
+        assert lat_min <= lat_max, 'ERROR: invalid lat'
+        assert lon_min <= lon_max, 'ERROR: invalid lon'
+        return '%.14f %.14f,%.14f %.14f,%.14f %.14f,%.14f %.14f,%.14f %.14f' % (lon_min, lat_min, lon_min, lat_max,
+                                                                                lon_max, lat_max, lon_max, lat_min,
+                                                                                lon_min, lat_min)
